79761587f702402836c8c745bf51392e21511979
StarbuckBarista <carlitos.salazar@protonmail.com>
2022-02-08 18:59:32 -0600

    Shooter Default Command
    Setup command execution to utilize the shooter subsystem functions, does not yet incorporate the indexer subsystem. Still need to put this command into the RobotContainer, waiting until indexer can be incorporated.

---
+++ src/main/java/frc/robot/commands/ShootWithInput.java
@@ -0,0 +1,53 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+import java.util.function.IntSupplier;
+
+/**
+ * This command is designed to be the default command for the
+ * shooter subsystem. It is used to shoot cargo during telop
+ * with driver input of shooting rings.
+ */
+// TODO indexer element, dependency, and incorporation
+public class ShootWithInput extends CommandBase {
+
+    private final Shooter shooter;
+    private final IntSupplier distanceSupplier;
+
+    /**
+     * @param shooter          the shooter subsystem used by this command
+     * @param distanceSupplier a supplier for changing shooting rings.
+     *                         often derived from joystick input but does not
+     *                         have to be.
+     */
+    public ShootWithInput (final Shooter shooter, final IntSupplier distanceSupplier) {
+
+        this.shooter = shooter;
+        this.distanceSupplier = distanceSupplier;
+
+        // use addRequirements() here to declare subsystem dependencies
+        addRequirements(this.shooter);
+    }
+
+    @Override
+    public void execute () {
+
+        this.shooter.cargoShot(this.distanceSupplier.getAsInt());
+    }
+
+    // driving with driver input never ends unless interrupted
+    @Override
+    public boolean isFinished () {
+
+        return false;
+    }
+
+    // called once the command ends or is interrupted
+    @Override
+    public void end (boolean interrupted) {
+
+        this.shooter.stop();
+    }
+}
