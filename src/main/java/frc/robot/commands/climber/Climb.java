df16fa04c67c2eb547b507e88af77ef12539957c
StarbuckBarista <carlitos.salazar@protonmail.com>
2022-02-26 14:33:05 -0600

    Made Climb Command Group
    Changed the climb command from being a regular command to be a command group.

--- src/main/java/frc/robot/commands/climber/Climb.java
+++ src/main/java/frc/robot/commands/climber/Climb.java
@@ -1,35 +1,22 @@
 package frc.robot.commands.climber;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Climber;
 
-public class Climb extends CommandBase {
+public class Climb extends SequentialCommandGroup {
     
     private final Climber climber;
-    private boolean finished;
 
     public Climb (final Climber climber) {
 
         this.climber = climber;
-        this.finished = false;
+
+        addCommands(
+            new RetractMotor(this.climber),
+            new SetPiston(this.climber, false),
+            new ExtendMotor(this.climber)
+        );
 
         addRequirements(this.climber);
-    }
-
-    @Override
-    public void execute () {
-
-        // TODO number of loops to run
-        new RetractMotor(this.climber).schedule();
-        new SetPiston(this.climber, false).schedule();
-        new ExtendMotor(this.climber).schedule();
-
-        this.finished = true;
-    }
-
-    @Override
-    public boolean isFinished () {
-
-        return this.finished;
     }
 }
