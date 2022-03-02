eb800eb75a8e54d223314d49219af08957bea3ef
1939DriverStation <barstowrobotics1939@gmail.com>
2022-03-02 15:43:02 -0600

    Removed Unnecessary Import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DriveWithInput;
 
 import frc.robot.commands.ToggleIntakeIndexerManualMode;
