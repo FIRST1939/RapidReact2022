9f3ccd6df918a015d01a69c5f9652f25be8b244e
Steve <sfwasleski@att.net>
2022-03-09 18:55:42 -0600

    Recover auto mode from bad branch

---
+++ src/main/java/frc/robot/commands/auto/AutoModeShooter.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.commands.indexer.IndexerReadyToShootState;
+import frc.robot.commands.indexer.IndexerShootingState;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.RobotCargoCount;
+import frc.robot.subsystems.Shooter;
+
+/**
+ * Used during autonomous mode to shoot 1 or 2 cargo when each is ready to be
+ * shot and the shooter is spun up.
+ */
+public class AutoModeShooter extends CommandBase {
+  private int shotsRemaining;
+  private final Indexer indexer;
+  private final Shooter shooter;
+
+  /**
+   * This command just monitors the indexer and triggers the shooter. It does not
+   * change their states directly and therfore does not require them.
+   * 
+   * <p>
+   * NOTE: The command group that defines the autonomous command MUST set the shot
+   * as early as possible before this command runs.
+   * </p>
+   * 
+   * @param shots   the number of cargo to fire. Will be forced to be less than or
+   *                equal to the number of cargo in the robot.
+   * @param indexer the indexer being monitored.
+   * @param shooter the shooter that will be triggered.
+   */
+  public AutoModeShooter(final int shots, final Indexer indexer, final Shooter shooter) {
+    this.shotsRemaining = Math.min(shots, RobotCargoCount.getInstance().get());
+    this.indexer = indexer;
+    this.shooter = shooter;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if ((shotsRemaining > 0)
+        && (this.indexer.getCurrentCommand() instanceof IndexerReadyToShootState)
+        && (this.shooter.isShooterReady())) {
+      IndexerShootingState.getInstance(this.indexer).schedule();
+      shotsRemaining--;
+    }
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return !this.indexer.isCargoAtSensor() && (this.shotsRemaining <= 0);
+  }
+}
---
+++ src/main/java/frc/robot/commands/auto/LeftSide2CargoNoTrajectory.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.commands.intake.IntakeGatheringEmptyState;
+import frc.robot.commands.shooter.SetShot;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+/**
+ * This auto command is to be set up in the left tarmac at and square to the
+ * backline (just to the left of the angle furthest from the hub). This should
+ * place it directly across from an alliance color cargo with the intake facing
+ * the cargo.
+ */
+public class LeftSide2CargoNoTrajectory extends SequentialCommandGroup {
+  /** Creates a new LeftSide2CargoNoTrajectory. */
+  public LeftSide2CargoNoTrajectory(
+      final DriveTrain driveTrain,
+      final Intake intake,
+      final Indexer indexer,
+      final Shooter shooter) {
+    addCommands(
+        // Configurable wait for alliance partner.
+        new WaitCommand(SmartDashboard.getNumber("Auto Start Wait", 0.0)),
+        // Gather, move to cargo and set for fender high.
+        new ParallelCommandGroup(
+            IntakeGatheringEmptyState.getInstance(intake),
+            new DriveStraightDistance(48.00, driveTrain),
+            new SetShot(shooter, Constants.SHOTS.fenderHigh)),
+        // Drive to point straight out from the fender.
+        new DriveStraightDistance(-100.0, driveTrain),
+        // Turn square to the fender.
+        new DriveTurnToRelativeAngle(-22.5, driveTrain),
+        // Drive to fender with timeout because we may hit and not reach distance.
+        new DriveStraightDistance(-50.0, driveTrain).withTimeout(3),
+        // Shoot with timeout in case of jam.
+        new AutoModeShooter(2, indexer, shooter).withTimeout(5),
+        // Exit tarmac.
+        new DriveStraightDistance(150.0, driveTrain));
+  }
+}
