233570ddb6f87d3d7fd17f42ce9c55ca9aacdbea
StarbuckBarista <66986004+StarbuckBarista@users.noreply.github.com>
2022-03-15 22:29:08 -0700

    Made Separate Command to Rumble Controller

---
+++ src/main/java/frc/robot/commands/RumbleController.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.climber;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Timer;
+
+public class RumbleController extends CommandBase {
+    
+    private final GenericHID controller;
+    private boolean finished = false;
+
+    public RumbleController (final GenericHID controller) {
+
+        this.controller = controller;
+    }
+
+    @Override
+    public void execute (boolean interrupted) {
+        
+        this.controller.setRumble(GenericHID.RumbleType.kLeftRumble, 1.0);
+        this.controller.setRumble(GenericHID.RumbleType.kRightRumble, 1.0);
+        
+        Timer.delay(0.1);
+        
+        this.controller.setRumble(GenericHID.RumbleType.kLeftRumble, 0.0);
+        this.controller.setRumble(GenericHID.RumbleType.kRightRumble, 0.0);
+      
+        this.finished = true;
+    }
+
+    @Override
+    public boolean isFinished () {
+
+        return this.finished;
+    }
+}
