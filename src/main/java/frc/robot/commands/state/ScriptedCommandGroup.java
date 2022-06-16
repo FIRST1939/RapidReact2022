b48eabcc3f1c8518c24d59975edf9bb76d2897cc
Steve <sfwasleski@att.net>
2022-06-16 11:31:46 -0400

    Javadoc

--- src/main/java/frc/robot/commands/state/ScriptedCommandGroup.java
+++ src/main/java/frc/robot/commands/state/ScriptedCommandGroup.java
@@ -6,12 +6,61 @@ package frc.robot.commands.state;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-/** Add your docs here. */
+/**
+ * An extension of {@link RandomAccessCommandGroup} that runs a script of
+ * command indexes. The script can be changed whenever the command group is not
+ * running. The currently set script will be used on the next command group
+ * execution. The default script is empty.
+ * 
+ * <p>
+ * Note that the script may contain a given command index 0, 1, or more times.
+ * 
+ * <p>
+ * The primary motivation for this class is to execute a terminating command
+ * group. This is particularly useful for autonomous commands or short lived
+ * bits of automation used during teleop. Since the script can be changed
+ * between runs, this class is particularly useful for automation that uses a
+ * known set of steps but the order of steps may need to vary based on
+ * environmental conditions. For example, the used of known steps during
+ * autonomous mode that varies based on sensed field indicators at the start of
+ * the match.
+ * 
+ * <p>
+ * As a rule, CommandGroups require the union of the requirements of their
+ * component commands.
+ *
+ * <p>
+ * This class is provided by the NewCommands VendorDep TODO
+ */
 public class ScriptedCommandGroup extends RandomAccessCommandGroup {
     public static final int[] EMPTY_SCRIPT = {};
     private int[] m_script;
     private int m_currentScriptIndex = RandomAccessCommandGroup.NO_CURRENT_COMMAND;
 
+    /**
+     * Creates a new ScriptedCommandGroup. The given commands will be run in a
+     * scripted order, with the CommandGroup finishing when no command is
+     * selected to run next, that is, the end of the script is reached.
+     * 
+     * <p>
+     * Note that execution always starts at the beginning of the script. Therefore,
+     * the {@link #setInitialCommandIndex(int)} is overridden to do nothing.
+     * 
+     * <p>
+     * The only time a contained command's end() method receives true for the
+     * interrupted parameter is when it is the current command and the entire group
+     * is interrupted. All other contained command end() calls will be due to the
+     * contained command's isFinish() returning true, and then the end() interrupted
+     * parameter will be false.
+     * 
+     * <p>
+     * Typically, if the end of the script is reached, this command group ends.
+     * However, this command is designed to work properly with
+     * {@link PerpetualCommand} (see {@link #perpetually()}). In that case, it will
+     * be restarted from the beginning of the script.
+     *
+     * @param commands the commands to include in this group.
+     */
     public ScriptedCommandGroup(final Command... commands) {
         // Would prefer to send an IntUnaryOperator on super call, but that would
         // require an instance method reference during construction which is not
